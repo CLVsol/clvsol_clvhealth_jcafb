3fc8adbdd29a5ab4d017dae715681b1afec71e26
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-05-23 20:09:45 +0000

    [IMP] data/setup.py (add clv_person_log_import_sqlite)

--- data/setup.py
+++ data/setup.py
@@ -214,15 +214,15 @@ def jcafb_2017_import_sqlite(client, db_path, conn_string):
     # print()
     # clv_address_import_sqlite(client, address_args, db_path, table_name, global_tag_table_name, category_table_name)
 
-    address_log_args = []
-    table_name = 'clv_address_log'
-    address_table_name = 'clv_address'
-    res_users_table_name = 'res_users'
-    print('-->', client, address_log_args, db_path, table_name, address_table_name, res_users_table_name)
-    print('--> Executing clv_address_log_import_sqlite()...')
-    print()
-    clv_address_log_import_sqlite(client, address_log_args, db_path, table_name,
-                                  address_table_name, res_users_table_name)
+    # address_log_args = []
+    # table_name = 'clv_address_log'
+    # address_table_name = 'clv_address'
+    # res_users_table_name = 'res_users'
+    # print('-->', client, address_log_args, db_path, table_name, address_table_name, res_users_table_name)
+    # print('--> Executing clv_address_log_import_sqlite()...')
+    # print()
+    # clv_address_log_import_sqlite(client, address_log_args, db_path, table_name,
+    #                               address_table_name, res_users_table_name)
 
     # person_category_args = []
     # table_name = 'clv_person_category'
@@ -252,6 +252,16 @@ def jcafb_2017_import_sqlite(client, db_path, conn_string):
     # clv_person_import_sqlite(
     #     client, person_args, db_path, table_name, global_tag_table_name, category_table_name, address_table_name
     # )
+
+    person_log_args = []
+    table_name = 'clv_person_log'
+    person_table_name = 'clv_person'
+    res_users_table_name = 'res_users'
+    print('-->', client, person_log_args, db_path, table_name, person_table_name, res_users_table_name)
+    print('--> Executing clv_person_log_import_sqlite()...')
+    print()
+    clv_person_log_import_sqlite(client, person_log_args, db_path, table_name,
+                                 person_table_name, res_users_table_name)
 
     # person_address_history_args = []
     # table_name = 'clv_person_address'
