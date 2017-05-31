61c5d188cf1eab90469c678a0d7a86a493ef49ff
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-05-31 13:12:03 +0000

    [IMP] project/install.py (add clv_mfile)

--- project/install.py
+++ project/install.py
@@ -523,6 +523,13 @@ def install():
     install_update_module('clv_event', update, group_names)
 
     group_names = [
+        'User (Media File)',
+        'Manager (Media File)',
+        'Super Manager (Media File)',
+    ]
+    install_update_module('clv_mfile', update, group_names)
+
+    group_names = [
         'User (Person)',
         'Manager (Person)',
         'Super Manager (Person)',
