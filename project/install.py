90abf049974101131f4848f85b2b63b8c0be5359
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-09-13 17:35:50 +0000

    [IMP] project/install.py (ADD clv_lab_test_jcafb_2018)

--- project/install.py
+++ project/install.py
@@ -663,6 +663,9 @@ def install():
     install_update_module('clv_lab_test_jcafb_2017', update, group_names)
 
     group_names = []
+    install_update_module('clv_lab_test_jcafb_2018', update, group_names)
+
+    group_names = []
     install_update_module('clv_mfile_jcafb', update, group_names)
 
     # # group_names = []
